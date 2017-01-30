9b768b664b7d475ead119a5ce0707e7c22c7b121
Andrew Suderman <andrew.suderman@readytalk.com>
2017-01-30 00:45:40 -0700

    Better plot on inverse kinematics

--- ikpy_test.py
+++ ikpy_test.py
@@ -8,19 +8,28 @@ from ikpy import plot_utils, geometry_utils
 
 my_chain = ikpy.chain.Chain.from_urdf_file("./poppy_ergo.URDF")
 
-target_vector = [ 0.1, -0.5, 0.1]
+target_vector = [ 0.1, -0.5, 0.2]
 target_frame = np.eye(4)
 target_frame[:3, 3] = target_vector
 
-print("The angles of each joints are : ", my_chain.inverse_kinematics(target_frame))
+print("The angles of each joints are : ",
+      my_chain.inverse_kinematics(target_frame))
 
-real_frame = my_chain.forward_kinematics(my_chain.inverse_kinematics(target_frame))
-print("Computed position vector : {0}\nOriginal position vector : {1}\n".format(real_frame[:3, 3], target_frame[:3, 3]))
+real_frame = my_chain.forward_kinematics(
+    my_chain.inverse_kinematics(target_frame))
 
-print("target_frame: {0}".format(target_frame))
-print("real_frame: {0}".format(real_frame))
+print("Computed position vector : {0}\nOriginal position vector : {1}\n"
+      .format(real_frame[:3, 3], target_frame[:3, 3]))
 
-ax = matplotlib.pyplot.figure().add_subplot(111, projection='3d')
+# print("target_frame: {0}".format(target_frame))
+# print("real_frame: {0}".format(real_frame))
+
+fig = matplotlib.pyplot.figure(figsize=(15, 9))
+
+ax = Axes3D(fig)
+ax.set_xlim3d(0, 1)
+ax.set_ylim3d(0, 1)
+ax.set_zlim3d(0, 1)
 
 my_chain_plot = my_chain.plot(my_chain.inverse_kinematics(target_frame), ax)
 
