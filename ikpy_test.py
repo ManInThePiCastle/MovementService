d0eb3901ba25ed88d6079da907d9e899e1d1afe9
Andrew Suderman <andrew.suderman@readytalk.com>
2017-01-30 01:01:09 -0700

    tweaking the inverse kinematics sample

--- ikpy_test.py
+++ ikpy_test.py
@@ -6,9 +6,10 @@ import matplotlib.pyplot
 from mpl_toolkits.mplot3d import Axes3D
 from ikpy import plot_utils, geometry_utils
 
+# Using example URDF right now.  Need to try to make one for THOR
 my_chain = ikpy.chain.Chain.from_urdf_file("./poppy_ergo.URDF")
 
-target_vector = [ 0.1, -0.5, 0.2]
+target_vector = [0.1, -0.5, 0.2]
 target_frame = np.eye(4)
 target_frame[:3, 3] = target_vector
 
@@ -25,12 +26,11 @@ print("Computed position vector : {0}\nOriginal position vector : {1}\n"
 # print("real_frame: {0}".format(real_frame))
 
 fig = matplotlib.pyplot.figure(figsize=(15, 9))
+fig.suptitle('Arm Position')
 
 ax = Axes3D(fig)
-ax.set_xlim3d(0, 1)
-ax.set_ylim3d(0, 1)
-ax.set_zlim3d(0, 1)
 
 my_chain_plot = my_chain.plot(my_chain.inverse_kinematics(target_frame), ax)
+
 
 matplotlib.pyplot.show()
