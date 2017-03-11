c3e0972dcd89ffe0d598ef67f2be04b37c7d29ad
Andrew Suderman <andrew.suderman@readytalk.com>
2017-03-11 02:12:21 +0000

    Troubleshooting

--- ManualControl/Thor_Control.py
+++ ManualControl/Thor_Control.py
@@ -5,6 +5,7 @@ import time
 import atexit
 import connexion
 import logging
+import copy
 
 class motor(object):
     """
@@ -28,14 +29,22 @@ class art(object):
         An articulation is one or more motors with a default direction
         EX: art([[motor1,0],[motor2,1]])
     """
-    def __init__(self, motors):
-        self._motors = motors
+    def __init__(self):
+        #self._motors = copy.deepcopy(motors)
+        self._motors = []
     
     @classmethod
+    def addmotor(self, motor_number, reverse):
+        self._motors.append([motor_number, reverse])
+        print(self._motors)
+
+    @classmethod
     def move_steps(self, reverse: int, steps):
+        motor_dict = {1: m1, 2: m2, 3: m3, 4: m4, 5: m5, 6: m6, 7: m7}
+        print(self._motors)
         for step in range(steps):
             for motor, def_direction in self._motors:
-                motor.step(bool(reverse) ^ bool(def_direction))
+                motor_dict[motor].step(bool(reverse) ^ bool(def_direction))
 
 m1 = motor(0x62, 2)
 m2 = motor(0x63, 2)
@@ -45,7 +54,9 @@ m5 = motor(0x60, 2)
 m6 = motor(0x62, 1)
 m7 = motor(0x60, 1)
 
-art1 = art([[m1, 0]])
+art1 = art()
+art1.addmotor(1,0)
+
 art2 = art([[m2, 0], [m3, 0]])
 art3 = art([[m4, 0]])
 art4 = art([[m5, 0]])
@@ -67,8 +78,7 @@ def post_manualcontrol(articulation: int, reverse: int, numsteps: int) -> str:
 
 if __name__ == '__main__':
     art_dict = {1: art1, 2: art2, 3: art3, 4: art4, 5: art5, 6: art6}
-    test.move_steps(0,1)
-    #art_dict[1].move_steps(0, 1)              
+    art_dict[1].move_steps(0, 1)              
     #app = connexion.App(__name__, 8080, specification_dir='swagger/')
     #app.add_api('thor_control.yaml', arguments={'title': 'Thor Arm Control'})
     app.run()
