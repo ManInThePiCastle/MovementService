990765481b3673322aff8cf2d9f9a33717773bae
Andrew Suderman <andrew.suderman@readytalk.com>
2017-03-11 15:03:35 -0700

    Updating gripper code

--- gripper.py
+++ gripper.py
@@ -1,9 +1,8 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 import time
 import wiringpi
 import curses
-
 
 wiringpi.wiringPiSetupGpio()
 
@@ -23,11 +22,15 @@ wiringpi.pwmWrite(18, pulse)
 
 # Minimum pulse is 85 (Gripper Open)
 # Maximum pulse is 185 (Gripper Closed)
-max_pulse = 185
-min_pulse = 85
+MAX_PULSE = 185
+MIN_PULSE = 85
 
 
 def input_char(message):
+    """
+       Takes an input of a char from the input stream.  
+       Allows for manual testing
+    """
     try:
         win = curses.initscr()
         win.addstr(0, 0, message)
@@ -42,51 +45,50 @@ def input_char(message):
 
 
 def open_close_gripper(command):
+    """
+        Opens or closes the gripper
+    """
     if command == 'close':
-        wiringpi.pwmWrite(18, max_pulse)
+        wiringpi.pwmWrite(18, MAX_PULSE)
     if command == 'open':
-        wiringpi.pwmWrite(18, min_pulse)
+        wiringpi.pwmWrite(18, MIN_PULSE)
     time.sleep(delay_period)
 
+
 def change_pulse(current_pulse, direction):
+    """
+        Changes the puluse up or down
+        Basically moves the servo in a direction
+    """
     print("Trying to move pulse {0} {1} by  {2}".format(str(current_pulse), str(direction), str(pulse_delta))) 
     if direction == 'up':
-        if current_pulse > max_pulse - pulse_delta:
+        if current_pulse > MAX_PULSE - pulse_delta:
             print('Cannot increment further')
             return current_pulse
         else:
             return current_pulse + pulse_delta
     if direction == 'down':
-        if current_pulse < min_pulse + pulse_delta:
+        if current_pulse < MIN_PULSE + pulse_delta:
             print('Cannot decrement further')
             return current_pulse
         else:
             return current_pulse - pulse_delta
             
-
-while True:
-    keystroke = raw_input("Command (q to quit):")
-    if keystroke.lower() == 'q':
-        break
-    elif keystroke == 'up':
-        pulse = change_pulse(pulse, 'up') 
-        wiringpi.pwmWrite(18, pulse)
-        time.sleep(delay_period)
-    elif keystroke == 'down':
-        pulse = change_pulse(pulse, 'down')
-        wiringpi.pwmWrite(18, pulse)
-        time.sleep(delay_period)
-    else:
-        open_close_gripper(keystroke)
-
-    print(str(pulse) + '\n')
-   #
-   # for pulse in range(50,250,1):
-   #     print(pulse)
-   #     wiringpi.pwmWrite(18, pulse)
-   #     time.sleep(delay_period)
-   # for pulse in range(250,50,-1):
-   #     print(pulse)
-   #     wiringpi.pwmWrite(18, pulse)
-   #     time.sleep(delay_period)
+if __name__ == '__main__':
+    while True:
+        keystroke = input("Command (q to quit):")
+        if keystroke.lower() == 'q':
+            break
+        elif keystroke == 'up':
+            pulse = change_pulse(pulse, 'up') 
+            wiringpi.pwmWrite(18, pulse)
+            time.sleep(delay_period)
+        elif keystroke == 'down':
+            pulse = change_pulse(pulse, 'down')
+            wiringpi.pwmWrite(18, pulse)
+            time.sleep(delay_period)
+        else:
+            open_close_gripper(keystroke)
+    
+        print(str(pulse) + '\n')
 
